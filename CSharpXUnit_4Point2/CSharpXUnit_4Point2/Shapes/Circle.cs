6a10de9cab699c3355f920c606e3ae1e56c15fa9
Hitesh Patel <hbp976@gmail.com>
2020-10-27 09:58:20 -0600

    Shapes class copied and namespace changed

---
+++ CSharpXUnit_4Point2/CSharpXUnit_4Point2/Shapes/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpXUnit_4Point2.Shapes
+{
+    // Pillar: Inheritance
+    class Circle : Shape
+    {
+        /*
+         “Perimeter” is equivalent to a circumference for “Circle”.
+        Properties:
+            ✅ “Radius”
+            ✅ “Diameter”
+                ✅ Public getter that derives its value from “Radius”.
+        */
+        public override double Perimeter => Math.PI * Diameter;
+
+        public override double Area => Math.PI * Math.Pow(Radius, 2);
+
+        public double Radius { get; set; }
+
+        public double Diameter
+        {
+            get => Radius * 2;
+            set
+            {
+                Radius = value / 2;
+            }
+        }
+
+        public Circle() : base()
+        {
+            Radius = 20;
+        }
+
+        public Circle(ColourValue colour, double radius) : base(colour)
+        {
+            Radius = radius;
+        }
+    }
+}
